1c6697be870968e35b3be713a1fa93ced4f1143d
elisabethkuning <ekuning@tktk.ee>
2023-02-08 21:36:11 +0200

    add week3ifandswitch code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int result = userNum % 2;
+
+//Console.WriteLine(result); //kui number on paaris, siis tulemus on 0, kui paaritu, siis ei ole 0
+
+if(result != 0) // != - ei ole võrdne
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
